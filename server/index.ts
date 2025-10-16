2edadd8dfc43806bb1742129b320f8bce6f48b57
Ethan Jin <ejin05@csu.fullerton.edu>
2025-10-16 11:55:24 -0700

    Git didn't pick this up earlier

--- server/index.ts
+++ server/index.ts
@@ -60,6 +60,7 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
+  // Updated
   const port = parseInt(process.env.PORT || '5000', 10);
   const host = process.platform === "win32" ? "localhost" : "0.0.0.0";
   server.listen({
