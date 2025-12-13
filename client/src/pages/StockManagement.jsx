dbad3ca0367830293c0ae60ca5a9bc23a99e5af8
jinjaebread <notchhammer@gmail.com>
2025-12-13 15:15:04 -0800

    Created Stock Market blank page & routed it to App.jsx & added to header.jsx

---
+++ client/src/pages/StockManagement.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+
+export default function StockManagement() {
+    return (
+        <div id="stock" className="min-h-screen bg-background">
+            Lipsum Irum
+        </div>
+      );
+}
+
